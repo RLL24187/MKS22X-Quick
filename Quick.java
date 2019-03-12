2f7bad6f2a8b1eaa74dd2c8681e18b27e80b8a0a
rleongPd5 <rleong00@stuy.edu>
2019-03-12 11:46:52 -0400

    Added toString, swap, and partition

---
+++ Quick.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+public class Quick{
+  public static int[] partition(int[] ary){
+    //select a random pivot
+    Random rnd = new Random();
+    int pivot = rnd.nextInt(ary.length);
+    System.out.println("Pivot value: "+ary[pivot]);
+    //move the pivot to the first index
+    //swap(pivot, 0, ary);
+    //loop through the array and partition into less than and greater than sections based on pivot
+    for (int i = 1; i < ary.length; i++){
+      if (ary[i]<ary[pivot]){
+        swap(i, 0, ary);
+      }
+      else if(ary[i]<ary[pivot]){
+        swap(i, ary.length-1, ary);
+      }
+    }
+    return ary;
+  }
+
+  //swaps values
+  private static void swap(int selectedIndex, int swapToIndex, int[] ary){
+    int temp = ary[swapToIndex];
+    ary[swapToIndex]=ary[selectedIndex];
+    ary[selectedIndex]=temp;
+  }
+
+  public static String toString(int[] ary){
+    String output = "{";
+    for (int i = 0; i < ary.length; i++){
+      output += ary[i];
+      if (i != ary.length - 1){
+        output+=", ";
+      }
+    }
+    return output +"}";
+  }
+  //methods static
+}
