2c206d69973eab28f61e957269835b0aa7d0cb9f
rleongPd5 <rleong00@stuy.edu>
2019-03-13 11:36:43 -0400

    Got partition to work!

--- Quick.java
+++ Quick.java
@@ -4,17 +4,23 @@ public class Quick{
     //select a random pivot
     Random rnd = new Random();
     int pivot = rnd.nextInt(ary.length);
-    System.out.println("Pivot value: "+ary[pivot]);
     //move the pivot to the first index
-    //swap(pivot, 0, ary);
+    swap(pivot, 0, ary);
+    pivot = 0;
     //loop through the array and partition into less than and greater than sections based on pivot
-    for (int i = 0; i < ary.length; i++){
-      if (ary[i]<ary[pivot]){
+    for (int i = 1; i < ary.length && pivot < ary.length; i++){
+      System.out.println("ary["+i+"]: "+ary[i]);
+      System.out.println("ary[i]<ary[pivot]: "+(ary[i]<ary[pivot]));
+      System.out.println("Pivot value: "+ary[pivot]);
+      System.out.println("Pivot: "+pivot);
+      if (ary[i]<=ary[pivot]){
         moveFront(i, ary);
+        System.out.println(toString(ary));
+        pivot++;
       }
-      else if (ary[i]>ary[pivot]){
-        moveBack(i, ary);
-      }
+      //else if (ary[i]>ary[pivot]){
+      //  moveBack(i, ary);
+      //}
       //else if(ary[i]<ary[pivot]){
       //  swap(i, ary.length-1, ary);
       //}
