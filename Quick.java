81e3a9220eb1e6d219ce350087775ef1b7eb9fc2
rleongPd5 <rleong00@stuy.edu>
2019-03-13 11:48:21 -0400

    Trying to optimize partition

--- Quick.java
+++ Quick.java
@@ -1,29 +1,31 @@
-import java.util.Random;
+//import java.util.Random;
 public class Quick{
   public static int[] partition(int[] ary){
     //select a random pivot
-    Random rnd = new Random();
-    int pivot = rnd.nextInt(ary.length);
+    //Random rnd = new Random();
+    //int pivot = rnd.nextInt(ary.length);
+
+    //Easy ( Implement both of these)
+    //a - When choosing a pivot, use the median value of the lo,hi, and middle elements.
+    //b - When a data element is equal to the pivot, make a 50% chance that you swap it to the other side.
+    //taking the median number as the pivot
+    int pivot = (ary[0]+ary[ary.length-1])/2;
+    System.out.println("Pivot: "+pivot);
     //move the pivot to the first index
-    swap(pivot, 0, ary);
-    pivot = 0;
+    //swap(pivot, 0, ary);
+    //pivot = 0;
     //loop through the array and partition into less than and greater than sections based on pivot
-    for (int i = 1; i < ary.length && pivot < ary.length; i++){
-      System.out.println("ary["+i+"]: "+ary[i]);
-      System.out.println("ary[i]<ary[pivot]: "+(ary[i]<ary[pivot]));
-      System.out.println("Pivot value: "+ary[pivot]);
-      System.out.println("Pivot: "+pivot);
-      if (ary[i]<=ary[pivot]){
+    for (int i = 1; i < ary.length; i++){
+      //System.out.println("ary["+i+"]: "+ary[i]);
+      //System.out.println("ary[i]<ary[pivot]: "+(ary[i]<ary[pivot]));
+      //System.out.println("Pivot value: "+ary[pivot]);
+      //System.out.println("Pivot: "+pivot);
+      //if (ary[i]<=ary[pivot]){
+      if (ary[i]<=pivot){
         moveFront(i, ary);
-        System.out.println(toString(ary));
-        pivot++;
+        //System.out.println(toString(ary));
+        //pivot++;
       }
-      //else if (ary[i]>ary[pivot]){
-      //  moveBack(i, ary);
-      //}
-      //else if(ary[i]<ary[pivot]){
-      //  swap(i, ary.length-1, ary);
-      //}
     }
     return ary;
   }
@@ -35,7 +37,7 @@ public class Quick{
     ary[selectedIndex]=temp;
   }
 
-  public static void moveFront(int selectedIndex, int[] ary){
+  private static void moveFront(int selectedIndex, int[] ary){
     int temp = ary[selectedIndex];
     for (int i = selectedIndex; i > 0; i--){
       ary[i]=ary[i-1];
@@ -43,7 +45,7 @@ public class Quick{
     ary[0]=temp;
   }
 
-  public static void moveBack(int selectedIndex, int[] ary){
+  private static void moveBack(int selectedIndex, int[] ary){
     int temp = ary[selectedIndex];
     for (int i = ary.length - 1; i > selectedIndex; i--){
       ary[i]=ary[i-1];
