33d514f7014079decd62483e7450394842356e7e
rleongPd5 <rleong00@stuy.edu>
2019-03-13 11:36:50 -0400

    Test case

--- Driver.java
+++ Driver.java
@@ -3,6 +3,9 @@ public class Driver{
     System.out.println("Testing partition");
     int[] a1 = {2, 4, 8, 1, 0, 3, 8, 7, 2};
     System.out.println(Quick.toString(a1));
+    System.out.println("Testing Quick.moveFront(5, a1)--> Move 3 to the front");
+    Quick.moveFront(5, a1);
+    System.out.println(Quick.toString(a1));
     Quick.partition(a1);
     System.out.println(Quick.toString(a1));
   }
