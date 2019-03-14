9120d1e4c2df479fe22ce537d3e5a34577a64ffa
rleongPd5 <rleong00@stuy.edu>
2019-03-14 06:13:38 -0400

    Added print statements for better reading

--- Driver.java
+++ Driver.java
@@ -17,20 +17,25 @@ public class Driver{
     System.out.println(Quick.toString(a1));
     //System.out.println("Testing Quick.moveFront(5, a1)--> Move 3 to the front");
     //Quick.moveFront(5, a1);
+    System.out.println("\nTesting (a1, 0, 8): ");
     System.out.println(Quick.toString(a1));
-    Quick.partition(a1);
+    Quick.partition(a1, 0, 8);
     System.out.println(Quick.toString(a1));
-
+    System.out.println("\nTesting (a2, 0, 8): ");
     System.out.println(Quick.toString(a2));
-    Quick.partition(a2);
+    Quick.partition(a2, 0, 8);
     System.out.println(Quick.toString(a2));
-
+    System.out.println("\nTesting (a3, 1, 1): ");
     System.out.println(Quick.toString(a3));
-    Quick.partition(a3);
+    Quick.partition(a3, 1, 1);
     System.out.println(Quick.toString(a3));
-
+    System.out.println("\nTesting (a4, 7, 12): ");
     System.out.println(Quick.toString(a4));
-    Quick.partition(a4);
+    Quick.partition(a4, 7, 12);
     System.out.println(Quick.toString(a4));
+    System.out.println("\nTesting (a5, 0, 14): ");
+    System.out.println(Quick.toString(a5));
+    Quick.partition(a5, 0, 14);
+    System.out.println(Quick.toString(a5));
   }
 }
