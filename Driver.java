8b48174c208f6aa7ef14a1430b4a0eb39a44b2bc
rleongPd5 <rleong00@stuy.edu>
2019-03-17 18:38:47 -0400

    Added new test cases

--- Driver.java
+++ Driver.java
@@ -1,16 +1,24 @@
 import java.util.*;
 public class Driver{
   public static void main(String[] args){
-    Random rnd = new Random(0);
+    Random rnd = new Random();
     int[] a2 = new int[10];
     int[] a3 = new int[10];
     int[] a4 = new int[15];
     int[] a5 = new int[15];
+    int[] a6 = new int[50];
+    int[] a7 = new int[100];
     for (int i = 0; i < a2.length; i++){
       a2[i] = rnd.nextInt(100);
       a3[i] = rnd.nextInt(100);
       a4[i] = rnd.nextInt(100);
       a5[i] = rnd.nextInt(100);
+    }
+    for (int i = 0; i < a7.length; i++){
+      if (i < a6.length){
+        a6[i]= rnd.nextInt(50);
+      }
+      a7[i]= rnd.nextInt(50);
     }
     System.out.println("Testing partition");
     int[] a1 = {2, 4, 8, 1, 0, 3, 8, 7, 2};
@@ -73,5 +81,34 @@ public class Driver{
     System.out.println("10: "+Quick.quickselect( ary , 3 ));//  would return 10
     System.out.println("15: "+Quick.quickselect( ary , 4 ));//  would return 15
     //System.out.println("23: "+Quick.quickselect( ary , 5 ));//  would return 23
+
+    System.out.println("\n---------Testing quickSort---------");
+    System.out.println("\na1\n"+Quick.toString(a1));
+    Quick.quicksort(a1);
+    System.out.println(Quick.toString(a1));
+
+    System.out.println("\na2\n"+Quick.toString(a2));
+    Quick.quicksort(a2);
+    System.out.println(Quick.toString(a2));
+
+    System.out.println("\na3\n"+Quick.toString(a3));
+    Quick.quicksort(a3);
+    System.out.println(Quick.toString(a3));
+
+    System.out.println("\na4\n"+Quick.toString(a4));
+    Quick.quicksort(a4);
+    System.out.println(Quick.toString(a4));
+
+    System.out.println("\na5\n"+Quick.toString(a5));
+    Quick.quicksort(a5);
+    System.out.println(Quick.toString(a5));
+
+    System.out.println("\na6\n"+Quick.toString(a6));
+    Quick.quicksort(a6);
+    System.out.println(Quick.toString(a6));
+
+    System.out.println("\na7\n"+Quick.toString(a7));
+    Quick.quicksort(a7);
+    System.out.println(Quick.toString(a7));
   }
 }
